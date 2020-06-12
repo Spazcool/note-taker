09cdbaaa02d7eb2fe6eb90fd905fb8d18f198cf6
Spazcool <douglas.wrightiii@gmail.com>
2020-06-11 22:24:08 -0400

    set up for heroku deploy

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const apiRoutes = require('./routes/apiRoutes.js');
 const htmlRoutes = require('./routes/htmlRoutes.js');
+const PORT = process.env.PORT || 3000;
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(58008)
+app.listen(PORT);
